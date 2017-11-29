93b7218c092a1f55bcd17976204b1663785e934c
Juan Rodriguez <sjdonado@uninorte.edu.co>
2017-11-29 00:45:36 -0500

    Pruebas angular

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Users in db';
+  data: any = null;
+  arrayUsers: any = null;
+  constructor(private http: Http) {}
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('http://localhost:3000/users').subscribe(data => {
+      // Read the result field from the JSON response.
+      console.log(data);
+      this.arrayUsers = Array.of(data.json().data)[0];
+      console.log(this.arrayUsers);
+    });
+  }
+/*   listUsers() {
+    this._http.get('http://localhost:3000/users').map((res: Response) => res.json()).subscribe(data => {
+      console.log(this.users(data));
+      console.log(data);
+    });
+  } */
+  private users(data): Object[] {
+    return Object.keys(data).map(function(index){
+      return data[index];
+    });
+  }
 }
